6e0c16f8c1baeb018c5c97902d6a73a45126fc7e
Hyun Jun <eppioes@gmail.com>
2022-06-21 14:15:56 +0900

    for GPU

--- Test.py
+++ Test.py
@@ -74,12 +74,14 @@ class DIRITester:
         state1 = self.agent.environment.observe()
         portfolio = self.agent.portfolio
         steps_done = 0
+        alphas = []
 
         while True:
-            action, confidence, log_prob = \
+            action, confidence, log_prob, alpha = \
                 self.agent.get_action(torch.tensor(state1, device=device).float().view(1, self.K, -1),
                                       torch.tensor(portfolio, device=device).float().view(1, self.K + 1, -1), self.repre)
 
+            alphas.append(alpha)
             #3일 단위로 거래
             if self.holding:
                 if steps_done % 3:
@@ -108,6 +110,8 @@ class DIRITester:
                 print(f"num buy:{self.num_buy}")
                 print(f"num sell:{self.num_sell}")
                 print(f"num hold:{self.num_hold}")
+                df = pd.DataFrame({"alpha":alphas})
+                df.to_csv("/content")
                 break
 
 
