b9f5ad5b25906dfecbc71d7a1b7d450760e6d679
Hyun Jun <eppioes@gmail.com>
2022-06-23 20:35:49 +0900

    for GPU

--- Network.py
+++ Network.py
@@ -81,7 +81,7 @@ class Actor(nn.Module):
             #     sampled_p = mode
 
             if True:
-                grid_seed = list(product(range(1, 11), repeat=N-1))
+                grid_seed = list(product(range(1, 10), repeat=N-1))
                 grid_seed = torch.tensor(grid_seed, device=device).float().view(-1, N-1)
                 cash_bias = torch.ones(size=(grid_seed.shape[0], 1), device=device) * 5.0
                 grid_seed = torch.cat([cash_bias, grid_seed], dim=-1)
