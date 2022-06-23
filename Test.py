0d3c2e11cb7b082ac80b27b537352b32f64fcf89
Hyun Jun <eppioes@gmail.com>
2022-06-23 17:02:36 +0900

    for GPU

--- Test.py
+++ Test.py
@@ -86,8 +86,8 @@ class DIRITester:
                 self.agent.get_action(torch.tensor(state1, device=device).float().view(1, self.K, -1),
                                       torch.tensor(portfolio, device=device).float().view(1, self.K + 1, -1), "mode")
 
-            action = 0.8 * action1 + 0.2 * action2
-            confidence = 0.8 * confidence1 + 0.2 * confidence2
+            action = 0.9 * action1 + 0.1 * action2
+            confidence = 0.9 * confidence1 + 0.1 * confidence2
 
             #3일 단위로 거래
             if self.holding:
