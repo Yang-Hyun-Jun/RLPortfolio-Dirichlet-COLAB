a585a7462366f4cf697046871932bfad57d5254e
Hyun Jun <eppioes@gmail.com>
2022-06-29 14:02:41 +0900

    for GPU

--- Network.py
+++ Network.py
@@ -101,10 +101,10 @@ class Actor(nn.Module):
 
         elif repre == "cost":
             now_port = utils.NOW_PORT
-            samples = dirichlet.sample(sample_shape=[500]).view(-1, N).cpu().numpy()
+            samples = dirichlet.sample(sample_shape=[1000]).view(-1, N).cpu().numpy()
             fees = [utils.check_fee((now_port - sample)[1:]) for sample in samples]
             fee_mean = utils.check_fee((now_port - dirichlet.mean.cpu().numpy())[1:])
-            fees.append(fee_mean)
+            # fees.append(fee_mean)
 
             min_ind = np.argmin(fees)
             min_por = samples[min_ind]
