d925021fd70ac7b5867bb99e9f9df778368ca39d
Hyun Jun <eppioes@gmail.com>
2022-06-30 01:23:42 +0900

    for GPU

--- Network.py
+++ Network.py
@@ -167,7 +167,7 @@ class Actor(nn.Module):
             sims_ = sims.copy()
             sims_.sort(reverse=True)
 
-            high_sim = sims_[:100]
+            high_sim = sims_[:30]
             high_ind = [sims.index(high) for high in high_sim]
             high_por = samples[high_ind]
 
@@ -201,21 +201,21 @@ class Actor(nn.Module):
             """
             피어슨 유사도 + 기대 수익률
             """
-            samples = dirichlet.sample(sample_shape=[10000]).view(-1, N).cpu()
+            samples = dirichlet.sample(sample_shape=[10000]).view(-1, N).cpu().numpy()
             mean = dirichlet.mean[0].cpu().numpy()
             sims = [np.dot((mean - np.mean(mean)), (sample - np.mean(sample))) \
                     / (norm(mean - np.mean(mean)) * norm(sample - np.mean(sample))) for sample in samples]
             sims_ = sims.copy()
             sims_.sort(reverse=True)
 
-            high_sim = sims_[:100]
+            high_sim = sims_[:30]
             high_ind = [sims.index(high) for high in high_sim]
             high_por = samples[high_ind]
 
             returns = [expected(utils.STOCK_LIST, torch.softmax(por[1:], dim=-1)) for por in high_por]
             max_ind = np.argmax(returns)
             max_por = high_por[max_ind]
-            sampled_p = max_por.to(device)
+            sampled_p = torch.tensor(max_por).to(device)
 
         elif repre is False:
             sampled_p = dirichlet.sample([1])[0]
