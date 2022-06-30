819819595e2b49e9748c1f6f3bc20d15fc259774
Hyun Jun <eppioes@gmail.com>
2022-06-30 11:39:28 +0900

    for GPU

--- Network.py
+++ Network.py
@@ -138,7 +138,6 @@ class Actor(nn.Module):
             """
             samples = dirichlet.sample(sample_shape=[10000]).view(-1, N).cpu().numpy()
             mean = dirichlet.mean[0].cpu().numpy()
-            mean = np.array([0.7, 0.1, 0.1, 0.1])
             sims = [dot(mean, sample)/(norm(mean) * norm(sample)) for sample in samples]
 
             max_ind = np.argmax(sims)
@@ -199,6 +198,25 @@ class Actor(nn.Module):
             sampled_p = min_por.to(device)
 
         elif repre == "cosmix3":
+            """
+            cos 유사도 + VaR
+            """
+            samples = dirichlet.sample(sample_shape=[10000]).view(-1, N).cpu()
+            mean = dirichlet.mean[0].cpu().numpy()
+            sims = [dot(mean, sample)/(norm(mean) * norm(sample)) for sample in samples]
+            sims_ = sims.copy()
+            sims_.sort(reverse=True)
+
+            high_sim = sims_[:10]
+            high_ind = [sims.index(high) for high in high_sim]
+            high_por = samples[high_ind]
+
+            vars = [VaR(utils.STOCK_LIST, torch.softmax(por[1:], dim=-1)) for por in high_por]
+            max_ind = np.argmax(vars)
+            max_por = high_por[max_ind]
+            sampled_p = max_por.to(device)
+
+        elif repre == "pearmix1":
             """
             피어슨 유사도 + 기대 수익률
             """
