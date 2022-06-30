4ef72e4438f72baa1981594bf01116d42314b9dd
Hyun Jun <eppioes@gmail.com>
2022-06-30 15:07:37 +0900

    for GPU

--- Network.py
+++ Network.py
@@ -277,9 +277,8 @@ class Actor(nn.Module):
             """
             mode + cos 유사도 + 기대 수익률 low
             """
-            dirichlet = dirichlet.to(device)
-            samples = dirichlet.sample(sample_shape=[10000]).view(-1, N)
-            logs = [dirichlet.log_prob(sample) for sample in samples]
+            samples = dirichlet.sample(sample_shape=[10000]).view(-1, N).cpu()
+            logs = [dirichlet.log_prob(sample).cpu() for sample in samples]
 
             high = samples[logs.index(max(logs))]
             sims = [dot(high, sample)/(norm(high) * norm(sample)) for sample in samples]
