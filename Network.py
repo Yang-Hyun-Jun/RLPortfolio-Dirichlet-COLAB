ea1dd485728adcc64e410a20666c72a6c3c73e7d
Hyun Jun <eppioes@gmail.com>
2022-07-01 15:26:46 +0900

    for GPU

--- Network.py
+++ Network.py
@@ -337,9 +337,9 @@ class Actor(nn.Module):
             # high_por = torch.tensor(high_por)
 
             returns = [variance(utils.STOCK_LIST, torch.softmax(torch.tensor(por[1:]), dim=-1)) for por in high_por]
-            # for _ in range(1):
-            #     returns.pop(np.argmin(returns))
-            #     high_por.pop(np.argmin(returns))
+            for _ in range(1):
+                returns.pop(np.argmin(returns))
+                high_por.pop(np.argmin(returns))
 
             min_ind = np.argmin(returns)
             min_por = high_por[min_ind]
