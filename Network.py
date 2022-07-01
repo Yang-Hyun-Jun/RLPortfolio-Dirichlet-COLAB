a1153779c9f0f9523f48b7f9d02e3c00c8c7b4f3
Hyun Jun <eppioes@gmail.com>
2022-07-01 16:13:06 +0900

    for GPU

--- Network.py
+++ Network.py
@@ -330,14 +330,14 @@ class Actor(nn.Module):
             sims_ = sims.copy()
             sims_.sort(reverse=True)
 
-            high_sim = sims_[:50]
+            high_sim = sims_[:500]
             high_ind = [sims.index(high) for high in high_sim]
             high_por = samples[high_ind]
             high_por = list(high_por)
             # high_por = torch.tensor(high_por)
 
             returns = [variance(utils.STOCK_LIST, torch.softmax(torch.tensor(por[1:]), dim=-1)) for por in high_por]
-            min_ind = np.argmax(returns)
+            min_ind = np.argmin(returns)
             min_por = high_por[min_ind]
             sampled_p = torch.tensor(min_por).to(device)
 
