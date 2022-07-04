10c75a5dd6e0873870d379a0a837186e7e79ddaf
Hyun Jun <eppioes@gmail.com>
2022-07-04 12:36:10 +0900

    for GPU

--- Network.py
+++ Network.py
@@ -211,7 +211,7 @@ class Actor(nn.Module):
             high_por = samples[high_ind]
 
             now_port = utils.NOW_PORT
-            fees = [utils.check_fee((high.numpy() - now_port)[1:])for high in high_por]
+            fees = [utils.check_fee((high.numpy() - now_port)[1:]) for high in high_por]
 
             min_ind = np.argmin(fees)
             min_por = high_por[min_ind]
@@ -357,9 +357,9 @@ class Actor(nn.Module):
             high_por = list(high_por)
 
             now_port = utils.NOW_PORT
-            fees = [utils.check_fee((high.numpy() - now_port)[1:])for high in high_por]
+            fees = [utils.check_fee((high.numpy() - now_port)[1:]) for high in high_por]
 
-            for _ in range(3):
+            for _ in range(6):
                 ind = np.argmin(fees)
                 fees.pop(ind)
                 high_por.pop(ind)
